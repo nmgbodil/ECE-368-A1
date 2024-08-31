4bf60f7d3d6a697a8fddfbb15b9ff3b3faa7ea8b
Nodebechukwu Julian Mgbodille <nmgbodil@eceprog3.ecn.purdue.edu>
2024-08-30 20:18:13 -0400

    Using argc, argv for input

--- src/main.c
+++ src/main.c
@@ -1,18 +1,18 @@
 #include "def.h"
 
-int main(void) {
+int main(int argc, char** argv) {
     // Variable Declarations
-    int cents;
-    int rtv;
+    // int cents;
+    // int rtv;
 
     // Get inputs
-    rtv = scanf("%d", &cents);
+    // rtv = scanf("%d", &cents);
 
-    if (!rtv) {
-        return EXIT_FAILURE;
-    }
+    // if (!rtv) {
+    //     return EXIT_FAILURE;
+    // }
 
-    get_combinations(cents);
+    get_combinations(atoi(argv[1]));
     return EXIT_SUCCESS;
 }
 
